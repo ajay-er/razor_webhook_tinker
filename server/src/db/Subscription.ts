b4f9057d2dc71ce98e28af4b76ef7c1dcb456145
ajay-er <ajayzyan100@gmail.com>
2023-12-15 16:41:13 +0530

    timestamp updated

--- server/src/db/Subscription.ts
+++ server/src/db/Subscription.ts
@@ -4,15 +4,20 @@ interface ISubscription extends Document {
   subscriptionId: string;
   amount: number;
   name: string;
-  email:string;
+  email: string;
 }
 
-const subscriptionSchema = new Schema<ISubscription>({
-  subscriptionId: { type: String , unique:true},
-  amount: { type: Number, required: true },
-  name: { type: String, required: true , default: 'no-name' },
-  email: { type: String, required: true },
-});
+const subscriptionSchema = new Schema<ISubscription>(
+  {
+    subscriptionId: { type: String, unique: true },
+    amount: { type: Number, required: true },
+    name: { type: String, required: true, default: "no-name" },
+    email: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const SubscriptionModel = mongoose.model<ISubscription>(
   "Subscription",
